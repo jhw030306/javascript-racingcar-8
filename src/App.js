d5c5c096b53706b32b1f549c7419b89d967bddd4
jhw030306 <jhw030306@naver.com>
2025-10-23 12:45:24 +0900

    feat(input) : 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+class errorHandler extends Error {
+  constructor(message) {
+    super(`[ERROR] ${message}`);
+    this.name = "StringCalculatorError";
+  }
+}
+
+async function inputName() {
+  const carInput = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+  );
+
+  const carName = carInput.split(",");
+  const cars = carName.map((name) => name.trim());
+
+  if (cars.some((x) => x.length > 5)) {
+    throw new errorHandler("자동차 이름이 깁니다.(5자 이하)");
+  }
+
+  const set = new Set(cars);
+
+  if (set.size != cars.length) {
+    throw new errorHandler("자동차 이름이 중복입니다.");
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    const cars = await inputName();
+  }
 }
 
 export default App;
