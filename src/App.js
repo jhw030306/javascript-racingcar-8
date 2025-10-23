88c7fb45baa12a1ee481d1d5a7bfcabe13e7fadf
jhw030306 <jhw030306@naver.com>
2025-10-23 17:43:38 +0900

    feat: 자동차 무작위 수 저장 시도

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import { inputCar } from "./input/InputCar.js";
 import { inputAttemptCount } from "./input/InputAttemptCount.js";
+
 class App {
   async run() {
     const carNames = await inputCar();
