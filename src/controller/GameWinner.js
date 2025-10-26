3d8f479829f70a8137828b9e573d45822c04e2ec
jhw030306 <jhw030306@naver.com>
2025-10-27 01:55:38 +0900

    fix : 오타 수정

--- src/controller/GameWinner.js
+++ src/controller/GameWinner.js
@@ -2,11 +2,15 @@ import { RacingCarView } from "../view/RacingCarView.js";
 
 export async function GameWinner(carNames, attemptCount) {
   const cars = await RacingCarView(carNames, attemptCount);
+
   const distances = cars.map((car) => car.position.length);
 
   const maxDistance = Math.max(...distances);
+
   const winner = cars
+
     .filter((car) => car.position.length === maxDistance)
+
     .map((car) => car.name);
 
   return winner;
--- src/input/InputCar.js
+++ src/input/InputCar.js
@@ -3,10 +3,12 @@ import Validator from "../validate/Validator.js";
 
 export async function inputCar() {
   const carInput = await Console.readLineAsync(
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
   );
 
-  const carNames = carInput.split(",").map((name) => name.trim());
+  const carNames = carInput
+    .split(",")
+    .map((name) => name.trim());
   Validator.validateCars(carNames);
   return carNames;
 }
