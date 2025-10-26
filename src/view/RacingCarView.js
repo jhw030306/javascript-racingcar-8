249b8be8cd79ab672ffd7300d79c04a842f8dde8
jhw030306 <jhw030306@naver.com>
2025-10-27 02:32:15 +0900

    fix : 오류 수정

--- src/view/RacingCarView.js
+++ src/view/RacingCarView.js
@@ -2,13 +2,9 @@ import { RacingCar } from "../controller/RacingCar.js";
 import { CarsPosition } from "../model/CarsPosition.js";
 import { Console } from "@woowacourse/mission-utils";
 
-export async function RacingCarView(
-  carNames,
-  attemptCount
-) {
-  const cars = await CarsPosition(carNames);
+export function RacingCarView(carNames, attemptCount) {
+  const cars = CarsPosition(carNames);
 
-  Console.print("");
   Console.print("실행 결과");
   for (let i = 0; i < attemptCount; i++) {
     RacingCar(cars);
