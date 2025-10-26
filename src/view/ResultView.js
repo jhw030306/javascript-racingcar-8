e701bd18a26fbf8babd2a002fa3a304f2798863c
jhw030306 <jhw030306@naver.com>
2025-10-27 02:33:41 +0900

    fix : 공백  수정

--- src/view/ResultView.js
+++ src/view/ResultView.js
@@ -3,5 +3,5 @@ import { Console } from "@woowacourse/mission-utils";
 
 export async function ResultView(carNames, attemptCount) {
   const winner = await GameWinner(carNames, attemptCount);
-  Console.print("최종 우승자 : " + winner.join(","));
+  Console.print(`최종 우승자 : ${winner.join(", ")}`);
 }
